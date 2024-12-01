523fe9292e9b45c99ff7c32bad62cf7e4f8aac07
lukas120121 <lukashedegaard99@gmail.com>
2024-12-01 16:25:06 +0100

    New augmentation in consep_dataset
    Use the new augmentation

--- hover_net/datasets/consep_dataset.py
+++ hover_net/datasets/consep_dataset.py
@@ -7,29 +7,16 @@ from shapely.geometry.polygon import Polygon
 from PIL import Image, ImageDraw
 
 import numpy as np
-from imgaug import augmenters as iaa
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
-from hover_net.dataloader.augmentation import (add_to_brightness,
-                                               add_to_contrast, add_to_hue,
-                                               add_to_saturation,
-                                               gaussian_blur, median_blur)
 from hover_net.dataloader.preprocessing import cropping_center, gen_targets
 
 from .hover_dataset import HoVerDatasetBase
 
 
 class CoNSePDataset(HoVerDatasetBase):
-    """Data Loader. Loads images from a file list and
-    performs augmentation with the albumentation library.
-    After augmentation, horizontal and vertical maps are
-    generated.
-    Args:
-        file_list: list of filenames to load
-        input_shape: shape of the input [h,w] - defined in config.py
-        mask_shape: shape of the output [h,w] - defined in config.py
-        mode: 'train' or 'valid'
-
-    """
+    """Data Loader using Albumentations for augmentations."""
 
     def __init__(
         self,
@@ -42,16 +29,13 @@ class CoNSePDataset(HoVerDatasetBase):
     ):
         assert input_shape is not None and mask_shape is not None
         self.run_mode = run_mode
-        # self.image_dir = image_dir
-        # self.geojson_dir = geojson_dir
 
         self.image_dir = 'data/01_training_dataset_tif_ROIs'
         self.geojson_dir = 'data/01_training_dataset_geojson_nuclei'
 
-        self.images     = os.listdir(self.image_dir)
-        self.geojsons   = os.listdir(self.geojson_dir)
+        self.images = os.listdir(self.image_dir)
+        self.geojsons = os.listdir(self.geojson_dir)
 
-        # Polygon class labels
         self.classes = {
             'nuclei_tumor': 0,  # Tumor
             'nuclei_lymphocyte': 1,  # TIL
@@ -68,31 +52,22 @@ class CoNSePDataset(HoVerDatasetBase):
         self.with_type = with_type
         self.mask_shape = mask_shape
         self.input_shape = input_shape
-        self.id = 0
         if setup_augmentor:
-            self.setup_augmentor(0, 0)
-        return
+            self.setup_augmentor(run_mode)
 
-    def setup_augmentor(self, worker_id, seed):
-        self.augmentor = self.__get_augmentation(self.run_mode, seed)
-        self.shape_augs = iaa.Sequential(self.augmentor[0])
-        self.input_augs = iaa.Sequential(self.augmentor[1])
-        self.id = self.id + worker_id
-        return
+    def setup_augmentor(self, mode):
+        self.augmentor = self.__get_augmentation(mode)
 
     def load_data(self, idx):
-        # Load image
         img_path = os.path.join(self.image_dir, self.images[idx])
         img = Image.open(img_path)
         width, height = img.size
 
-        # Create blank images to draw instances and annotations
-        instances   = Image.new(mode="L", size=(width, height))
+        instances = Image.new(mode="L", size=(width, height))
         annotations = Image.new(mode="L", size=(width, height))
         inst_draw = ImageDraw.Draw(instances)
         ann_draw = ImageDraw.Draw(annotations)
 
-        # Load GeoJSON
         json_path = os.path.join(self.geojson_dir, self.geojsons[idx])
         with open(json_path, encoding="utf-8") as f:
             geojson = json.load(f)
@@ -113,13 +88,9 @@ class CoNSePDataset(HoVerDatasetBase):
                 inst_draw.polygon(coords, outline=inst_id, fill=inst_id)
                 inst_id += 1
 
-        # Combine instances and annotations
         ann = np.stack([np.array(instances), np.array(annotations)], axis=0).astype("int32")
         ann = np.transpose(ann, (1, 2, 0))
         img = np.array(img).astype("uint8")[:, :, :3]
-
-        print(ann.shape)
-        print(img.shape)
 
         return img, ann
 
@@ -129,19 +100,15 @@ class CoNSePDataset(HoVerDatasetBase):
     def __getitem__(self, idx):
         img, ann = self.load_data(idx)
 
-        if self.shape_augs is not None:
-            shape_augs = self.shape_augs.to_deterministic()
-            img = shape_augs.augment_image(img)
-            ann = shape_augs.augment_image(ann)
-
-        if self.input_augs is not None:
-            input_augs = self.input_augs.to_deterministic()
-            img = input_augs.augment_image(img)
+        if self.augmentor is not None:
+            augmented = self.augmentor(image=img, mask=ann)
+            img = augmented["image"]
+            ann = augmented["mask"]
 
         img = cropping_center(img, self.input_shape)
         feed_dict = {"img": img}
 
-        inst_map = ann[..., 0]  # HW1 -> HW
+        inst_map = ann[..., 0]
         if self.with_type:
             type_map = (ann[..., 1]).copy()
             type_map = cropping_center(type_map, self.mask_shape)
@@ -152,96 +119,34 @@ class CoNSePDataset(HoVerDatasetBase):
 
         return feed_dict
 
-    def __get_augmentation(self, mode, rng):
+    def __get_augmentation(self, mode):
         if mode == "train":
-            shape_augs = [
-                # * order = ``0`` -> ``cv2.INTER_NEAREST``
-                # * order = ``1`` -> ``cv2.INTER_LINEAR``
-                # * order = ``2`` -> ``cv2.INTER_CUBIC``
-                # * order = ``3`` -> ``cv2.INTER_CUBIC``
-                # * order = ``4`` -> ``cv2.INTER_CUBIC``
-                # ! for pannuke v0, no rotation or translation,
-                # ! just flip to avoid mirror padding
-                # iaa.Affine(
-                #     # scale images to 80-120% of their size,
-                #     # individually per axis
-                #     scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
-                #     # translate by -A to +A percent (per axis)
-                #     translate_percent={
-                #         "x": (-0.01, 0.01), "y": (-0.01, 0.01)},
-                #     shear=(-5, 5),  # shear by -5 to +5 degrees
-                #     rotate=(-179, 179),  # rotate by -179 to +179 degrees
-                #     order=0,  # use nearest neighbour
-                #     backend="cv2",  # opencv for fast processing
-                #     seed=rng,
-                # ),
-                # set position to 'center' for center crop
-                # else 'uniform' for random crop
-                iaa.CropToFixedSize(
-                    self.input_shape[0], self.input_shape[1], position="center"
-                ),
-                iaa.Fliplr(0.5, seed=rng),
-                iaa.Flipud(0.5, seed=rng),
-            ]
-
-            input_augs = [
-                iaa.OneOf(
-                    [
-                        iaa.Lambda(
-                            seed=rng,
-                            func_images=lambda *args: gaussian_blur(
-                                *args, max_ksize=3
-                            ),
-                        ),
-                        iaa.Lambda(
-                            seed=rng,
-                            func_images=lambda *args: median_blur(
-                                *args, max_ksize=3
-                            ),
-                        ),
-                        iaa.AdditiveGaussianNoise(
-                            loc=0, scale=(0.0, 0.05 * 255), per_channel=0.5
-                        ),
-                    ]
-                ),
-                iaa.Sequential(
-                    [
-                        iaa.Lambda(
-                            seed=rng,
-                            func_images=lambda *args: add_to_hue(
-                                *args, range=(-8, 8)
-                            ),
-                        ),
-                        iaa.Lambda(
-                            seed=rng,
-                            func_images=lambda *args: add_to_saturation(
-                                *args, range=(-0.2, 0.2)
-                            ),
-                        ),
-                        iaa.Lambda(
-                            seed=rng,
-                            func_images=lambda *args: add_to_brightness(
-                                *args, range=(-26, 26)
-                            ),
-                        ),
-                        iaa.Lambda(
-                            seed=rng,
-                            func_images=lambda *args: add_to_contrast(
-                                *args, range=(0.75, 1.25)
-                            ),
-                        ),
-                    ],
-                    random_order=True,
-                ),
-            ]
+            aug = A.Compose(
+                [
+                    A.RandomCrop(height=self.input_shape[0], width=self.input_shape[1]),
+                    A.HorizontalFlip(p=0.5),
+                    A.VerticalFlip(p=0.5),
+                    A.OneOf(
+                        [
+                            A.GaussianBlur(blur_limit=(3, 3), p=0.5),
+                            A.MedianBlur(blur_limit=3, p=0.5),
+                            A.GaussNoise(var_limit=(10.0, 50.0), p=0.5),
+                        ],
+                        p=0.5,
+                    ),
+                    A.OneOf(
+                        [
+                            A.HueSaturationValue(hue_shift_limit=8, sat_shift_limit=8, val_shift_limit=8, p=0.5),
+                            A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
+                        ],
+                        p=0.5,
+                    ),
+                ]
+            )
         else:
-            shape_augs = [
-                # set position to 'center' for center crop
-                # else 'uniform' for random crop
-                iaa.CropToFixedSize(
-                    self.input_shape[0], self.input_shape[1], position="center"
-                )
-            ]
-            input_augs = []
-
-        return shape_augs, input_augs
+            aug = A.Compose(
+                [
+                    A.CenterCrop(height=self.input_shape[0], width=self.input_shape[1]),
+                ]
+            )
+        return aug
