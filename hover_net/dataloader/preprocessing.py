66612d1fe62830c3bfa4e8b75605abcc9d64b5a2
andreasp <andreas.s.p00@gmail.com>
2024-12-09 15:21:32 +0100

    update preprocessing

--- hover_net/dataloader/preprocessing.py
+++ hover_net/dataloader/preprocessing.py
@@ -1,33 +1,13 @@
+# Packages
 import numpy as np
-from scipy import ndimage
 from scipy.ndimage import measurements
-from skimage import morphology as morph
-
-
-def cropping_center(x, crop_shape, batch=False):
-    """Crop an input image at the centre.
-    Args:
-        x: input array
-        crop_shape: dimensions of cropped array
-
-    Returns:
-        x: cropped array
-
-    """
-    orig_shape = x.shape
-    if not batch:
-        h0 = int((orig_shape[0] - crop_shape[0]) * 0.5)
-        w0 = int((orig_shape[1] - crop_shape[1]) * 0.5)
-        x = x[h0:h0 + crop_shape[0], w0:w0 + crop_shape[1]]
-    else:
-        h0 = int((orig_shape[1] - crop_shape[0]) * 0.5)
-        w0 = int((orig_shape[2] - crop_shape[1]) * 0.5)
-        x = x[:, h0:h0 + crop_shape[0], w0:w0 + crop_shape[1]]
-    return x
-
 
 def get_bounding_box(img):
-    """Get bounding box coordinate information."""
+    """
+    Get bounding box coordinate information.
+
+    Source: https://github.com/vqdang/hover_net
+    """
     rows = np.any(img, axis=1)
     cols = np.any(img, axis=0)
     rmin, rmax = np.where(rows)[0][[0, -1]]
@@ -40,9 +20,11 @@ def get_bounding_box(img):
 
 
 def fix_mirror_padding(ann):
-    """Deal with duplicated instances due to mirroring in interpolation
+    """
+    Deal with duplicated instances due to mirroring in interpolation
     during shape augmentation (scale, rotation etc.).
 
+    Source: https://github.com/vqdang/hover_net
     """
     current_max_id = np.amax(ann)
     inst_list = list(np.unique(ann))
@@ -56,30 +38,23 @@ def fix_mirror_padding(ann):
     return ann
 
 
-def gen_instance_hv_map(ann, crop_shape):
-    """Input annotation must be of original shape.
-
-    The map is calculated only for instances within the crop portion
-    but based on the original shape in original image.
-
-    Perform following operation:
-    Obtain the horizontal and vertical distance maps for each
-    nuclear instance.
-
+def gen_instance_hv_map(ann):
     """
-    orig_ann = ann.copy()  # instance ID map
-    fixed_ann = fix_mirror_padding(orig_ann)
+    Generate the HoVer maps for each nuclear instance.
 
-    # re-cropping with fixed instance id map
-    crop_ann = cropping_center(fixed_ann, crop_shape)
+    Args:
+        - ann: instance ID map
+    Returns:
+        - hv_map: horizontal and vertical distance maps
 
-    # TODO: deal with 1 label warning
-    crop_ann = morph.remove_small_objects(crop_ann, min_size=30)
+    Based on: https://github.com/vqdang/hover_net
+    """
+    fixed_ann = fix_mirror_padding(ann)
 
-    x_map = np.zeros(orig_ann.shape[:2], dtype=np.float32)
-    y_map = np.zeros(orig_ann.shape[:2], dtype=np.float32)
+    x_map = np.zeros(ann.shape[:2], dtype=np.float32)
+    y_map = np.zeros(ann.shape[:2], dtype=np.float32)
 
-    inst_list = list(np.unique(crop_ann))
+    inst_list = list(np.unique(ann))
     inst_list.remove(0)  # 0 is background
     for inst_id in inst_list:
         inst_map = np.array(fixed_ann == inst_id, np.uint8)
@@ -140,14 +115,20 @@ def gen_instance_hv_map(ann, crop_shape):
     return hv_map
 
 
-def gen_targets(ann, crop_shape):
-    """Generate the targets for the network."""
-    hv_map = gen_instance_hv_map(ann, crop_shape)
+def gen_targets(ann):
+    """
+    Generate the targets for the network.
+    
+    Args:
+        - ann: instance ID map
+    Returns:
+        - target_dict: dictionary containing the hv_maps (X, Y, 2) and np_map (X, Y)
+
+    Based on: https://github.com/vqdang/hover_net
+    """
+    hv_map = gen_instance_hv_map(ann)
     np_map = ann.copy()
     np_map[np_map > 0] = 1
-
-    hv_map = cropping_center(hv_map, crop_shape)
-    np_map = cropping_center(np_map, crop_shape)
 
     target_dict = {
         "hv_map": hv_map,
@@ -155,46 +136,3 @@ def gen_targets(ann, crop_shape):
     }
 
     return target_dict
-
-
-def remove_small_objects(pred, min_size=64, connectivity=1):
-    """Remove connected components smaller than the specified size.
-
-    This function is taken from skimage.morphology.remove_small_objects,
-    but the warning is removed when a single label is provided.
-
-    Args:
-        pred: input labelled array
-        min_size: minimum size of instance in output array
-        connectivity: The connectivity defining the neighborhood of a pixel.
-
-    Returns:
-        out: output array with instances removed under min_size
-
-    """
-    out = pred
-
-    if min_size == 0:  # shortcut for efficiency
-        return out
-
-    if out.dtype == bool:
-        selem = ndimage.generate_binary_structure(pred.ndim, connectivity)
-        ccs = np.zeros_like(pred, dtype=np.int32)
-        ndimage.label(pred, selem, output=ccs)
-    else:
-        ccs = out
-
-    try:
-        component_sizes = np.bincount(ccs.ravel())
-    except ValueError:
-        raise ValueError(
-            "Negative value labels are not supported. Try "
-            "relabeling the input with `scipy.ndimage.label` or "
-            "`skimage.morphology.label`."
-        )
-
-    too_small = component_sizes < min_size
-    too_small_mask = too_small[ccs]
-    out[too_small_mask] = 0
-
-    return out
