a29bc035fb9743fa5c267e49bd2ebb48047b38f2
andreasp <andreas.s.p00@gmail.com>
2024-12-09 16:26:32 +0100

    update infer.py

--- hover_net/process/infer.py
+++ hover_net/process/infer.py
@@ -1,33 +1,55 @@
+# Core
 from collections import OrderedDict
 
+# Packages
 import torch
 import torch.nn.functional as F
 
+def infer_step(batch_imgs, model, device="cuda"):
+    """
+    Infer a batch of images using the HoVer-net model.
 
-def infer_step(batch_data, model, device="cuda"):
-    patch_imgs = batch_data
-
-    patch_imgs_gpu = patch_imgs.to(device).type(torch.float32)  # to NCHW
+    Args:
+        - batch_imgs: torch.Tensor(B, H, W, C)
+        - model: torch.nn.Module
+    
+    Returns:
+        - pred_output: np.array(B, H, W, C)
+            C=0: type map,
+            C=1: nuclear pixel map,
+            C=2: horizontal map,
+            C=3: vertical map
+    
+    Based on: https://github.com/Kaminyou/HoVer-Net-PyTorch
+    """
+    # Move images to gpu and permute to (B, C, H, W)
+    patch_imgs_gpu = batch_imgs.to(device).type(torch.float32)
     patch_imgs_gpu = patch_imgs_gpu.permute(0, 3, 1, 2).contiguous()
 
-    ####
-    model.eval()  # infer mode
+    # Put model in eval mode
+    model.eval()
 
-    # --------------------------------------------------------------
-    with torch.no_grad():  # dont compute gradient
+    # ... And DONT compute gradients
+    with torch.no_grad():
+        # Forward pass
+        # pred_dict: ordered dict with tp, np, hv keys
         pred_dict = model(patch_imgs_gpu)
+
+        # Post-process the output
+        # Permute back to (B, H, W, C)
         pred_list = []
         for k, v in pred_dict.items():
             pred_list.append([k, v.permute(0, 2, 3, 1).contiguous()])
         pred_dict = OrderedDict(pred_list)
+
+        # Softmax the nuclear pixel map
         pred_dict["np"] = F.softmax(pred_dict["np"], dim=-1)[..., 1:]
-        if "tp" in pred_dict:
-            type_map = F.softmax(pred_dict["tp"], dim=-1)
-            type_map = torch.argmax(type_map, dim=-1, keepdim=True)
-            type_map = type_map.type(torch.float32)
-            pred_dict["tp"] = type_map
+
+        # Softmax the type map
+        type_map = F.softmax(pred_dict["tp"], dim=-1)
+        type_map = torch.argmax(type_map, dim=-1, keepdim=True)
+        type_map = type_map.type(torch.float32)
+        pred_dict["tp"] = type_map
         pred_output = torch.cat(list(pred_dict.values()), -1)
 
-    # * Its up to user to define the protocol '
-    # to process the raw output per step!
     return pred_output.cpu().numpy()
