861ca4cfe475a80ce80a4bfe6f40d179e4ba5fed
andreasp <andreas.s.p00@gmail.com>
2024-12-10 11:42:05 +0100

    update setup scripts

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "Downloading PUMA dataset..."
 wget https://zenodo.org/records/14213079/files/01_training_dataset_geojson_nuclei.zip?download=1 -O data/01_training_dataset_geojson_nuclei.zip
 wget https://zenodo.org/records/14213079/files/01_training_dataset_tif_ROIs.zip?download=1 -O data/01_training_dataset_tif_ROIs.zip
 
-# Unzip the datasets
+# Unzip
 echo "Unzipping datasets..."
 unzip -o data/01_training_dataset_geojson_nuclei.zip -d data
 unzip -o data/01_training_dataset_tif_ROIs.zip -d data
