f8f124f7f6d20d697494e3e71b55a9b0f64f5868
andreasp <andreas.s.p00@gmail.com>
2024-12-10 15:09:12 +0100

    Revert "remove accumulated_output"
    This reverts commit d8776d489c0005acb1f475f3f00ecaea636a8e3c.

--- hover_net/tools/utils.py
+++ hover_net/tools/utils.py
@@ -1,5 +1,17 @@
 import yaml
 
+
+def update_accumulated_output(accumulated_output, step_output):
+    step_output = step_output["raw"]
+
+    for key, step_value in step_output.items():
+        if key in accumulated_output:
+            accumulated_output[key].extend(list(step_value))
+        else:
+            accumulated_output[key] = list(step_value)
+    return
+
+
 def read_yaml(yaml_path):
     with open(yaml_path, "r") as f:
         config = yaml.load(f, Loader=yaml.SafeLoader)
