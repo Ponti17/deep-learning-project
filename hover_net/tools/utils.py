fd66221315c7bc751aac4e76027648334f8ad26c
andreasp <andreas.s.p00@gmail.com>
2024-12-11 16:40:20 +0100

    update utils

--- hover_net/tools/utils.py
+++ hover_net/tools/utils.py
@@ -1,7 +1,11 @@
+# Core
 import yaml
 
-
 def update_accumulated_output(accumulated_output, step_output):
+    """
+    Just makes our life easier when we want to accumulate the output of the
+    validation.
+    """
     step_output = step_output["raw"]
 
     for key, step_value in step_output.items():
@@ -13,13 +17,19 @@ def update_accumulated_output(accumulated_output, step_output):
 
 
 def read_yaml(yaml_path):
-    with open(yaml_path, "r") as f:
+    """
+    Read a YAML file :)
+    """
+    with open(yaml_path, "r", encoding='utf-8') as f:
         config = yaml.load(f, Loader=yaml.SafeLoader)
     return config
 
 
 def dump_yaml(save_path, dictionary):
-    with open(save_path, "w") as f:
+    """
+    Dump it :(
+    """
+    with open(save_path, "w", encoding='utf-8') as f:
         yaml.dump(
             dictionary,
             f,
